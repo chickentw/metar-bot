63e2423d959fa5b9036ed4527f9b2b7fae44b9ff
doratown <wilson@touhaodeMacBook-Pro-Retina.local>
2015-10-05 05:27:08 +0800

    Add plurk support

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 A twitter bot that tweet aviation weather information
 
 This are to be use together with twitterOauth.
+
+Plurk are support edit plurk.php for API setting.
+
+Plurk PHP source from https://github.com/davidou123/plurk_api_2.0_demo_php
--- twitter.php
+++ twitter.php
@@ -1,23 +1,26 @@
 <?PHP
     //Created by doratown
 
-require_once 'twitteroauth.php';
-
+    require_once 'twitteroauth.php';
+    include("plurk.php");
+    
     define("CONSUMER_KEY", "Your key here");
     define("CONSUMER_SECRET", "Your key here");
     define("OAUTH_TOKEN", "Your key here");
     define("OAUTH_SECRET", "Your key here");
     
-$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, OAUTH_TOKEN, OAUTH_SECRET);
-$content = $connection->get('account/verify_credentials');
+    $connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, OAUTH_TOKEN, OAUTH_SECRET);
+    $content = $connection->get('account/verify_credentials');
+    
+    $qulifier='says';
 
-$id=$_SERVER["QUERY_STRING"];
-$url="http://www.aviationweather.gov/metar/data?ids=rctp&format=raw&date=0&hours=0";
-$info=file_get_contents($url);
-preg_match('/code>(.*?)</i',$info,$m);
+    $id=$_SERVER["QUERY_STRING"];
+    $url="http://www.aviationweather.gov/metar/data?ids=rctp&format=raw&date=0&hours=0";
+    $info=file_get_contents($url);
+    preg_match('/code>(.*?)</i',$info,$m);
 
 
     $connection->post('statuses/update', array('status' => $m[1]));
-
+    $post_info = do_action("http://www.plurk.com/APP/Timeline/plurkAdd",array("content"=>rawurlencode($m[1]),"qualifier"=>$qulifier));
 
 ?>
