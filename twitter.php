e7f33c25a9fa11e6caa53145c6336bc5ef294d8a
doratown <wilson@touhaodeMacBook-Pro-Retina.local>
2015-10-05 05:34:02 +0800

    Add explanations

--- twitter.php
+++ twitter.php
@@ -4,18 +4,18 @@
     require_once 'twitteroauth.php';
     include("plurk.php");
     
-    define("CONSUMER_KEY", "Your key here");
-    define("CONSUMER_SECRET", "Your key here");
-    define("OAUTH_TOKEN", "Your key here");
-    define("OAUTH_SECRET", "Your key here");
+    define("CONSUMER_KEY", "Your key here"); //key for twitter
+    define("CONSUMER_SECRET", "Your key here"); //key for twitter
+    define("OAUTH_TOKEN", "Your key here"); //key for twitter
+    define("OAUTH_SECRET", "Your key here"); //key for twitter
     
     $connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, OAUTH_TOKEN, OAUTH_SECRET);
     $content = $connection->get('account/verify_credentials');
     
-    $qulifier='says';
+    $qulifier='says'; //Plurk qulifier, Enlish Only
 
     $id=$_SERVER["QUERY_STRING"];
-    $url="http://www.aviationweather.gov/metar/data?ids=rctp&format=raw&date=0&hours=0";
+    $url="http://www.aviationweather.gov/metar/data?ids=rctp&format=raw&date=0&hours=0"; //Metar address, change the ICAO code
     $info=file_get_contents($url);
     preg_match('/code>(.*?)</i',$info,$m);
 
